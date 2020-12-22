9967b554974d3bbc9e235ac0705c227152286f20
Ewan Harris <ewanharris93@gmail.com>
2020-12-22 16:39:53 +0000

    build: fix eslint setup

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+	root: true,
+	parser: '@typescript-eslint/parser',
+	plugins: [
+		'jest',
+		'@typescript-eslint',
+	],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+	],
+	rules: {
+		'indent': [ 'error', 'tab' ]
+	}
+};
