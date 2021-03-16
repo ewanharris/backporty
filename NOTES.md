2bed6d62f72c6006cab7673d687d9b57dc0b4489
Ewan Harris <ewanharris93@gmail.com>
2021-03-16 22:11:28 +0000

    fix: more bad syntax

---
+++ NOTES.md
@@ -0,0 +1,6 @@
+## Features
+
+tropkcab exposes a few different features that you can toggle:
+
+* Backport Strategy
+* Resolve lockfile conflicts
--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,4 @@
-import core from '@actions/core';
+import * as core from '@actions/core';
 import { ActionArguments } from './interfaces';
 
 export function validateArgs(): ActionArguments {
